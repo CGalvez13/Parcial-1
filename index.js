f3858fc48e0ec3b067c012748b7f91e62110db18
Carlos Gálvez Gómez <danyboy@Carloss-MacBook-Pro.local>
2022-03-27 15:41:56 -0600

    Api controller #4

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-function GetClima() {
+const APIController = (function() {
 
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
+      const clientId = 'b26d24a2d68d4e6a83fb64206a1fd936';
+      const clientSecret = '0b53f980050943469c1d4336f6ec9ad2';
 
-fetch("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=09e0ef7219cfe8ad1f7067468fd3a5a2", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+      //metodos privados
+      const _getToken = async () => {
+
+        const result = await fetch ('https://acounts.spotify.com/api/token', {
+          method: 'POST',
+          headers: {
+              'Content-Type' : 'aplication/x-www-form-urlencoded',
+              'Authorization' : 'Basic' +btoa( clientId + ':' + clientSecret)
+          },
+          body: 'grant_type=client_credentials'
+        });
+
+        const data = await result.json();
+        return data.acess_token;
+      }
+
 }
+)
